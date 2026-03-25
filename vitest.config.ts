ba7e028f561ada1472ded98c28ea185093a3b76e
C. Spencer Beggs <spencer@beggs.codes>
2026-03-25 01:38:56 -0400

    chore: update internals
    Signed-off-by: C. Spencer Beggs <spencer@beggs.codes>

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,3 @@
 import { VitestConfig } from "@savvy-web/vitest";
 
-export default VitestConfig.create(({ projects, coverage, reporters }) => ({
-	test: {
-		reporters,
-		projects: projects.map((p) => p.toConfig()),
-		coverage: { provider: "v8", ...coverage },
-	},
-}));
+export default VitestConfig.create();
